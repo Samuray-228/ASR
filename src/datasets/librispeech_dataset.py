af9c924dfc3ad05ca2b89e2cdde69684defedb9b
Samuray-228 <leonkv04@mail.ru>
2025-10-16 15:56:21 +0300

    dataset fix

--- src/datasets/librispeech_dataset.py
+++ src/datasets/librispeech_dataset.py
@@ -43,16 +43,6 @@ class LibrispeechDataset(BaseDataset):
 
         super().__init__(index, *args, **kwargs)
         
-    def _load_part(self, part):
-        arch_path = self._data_dir / f"{part}.tar.gz"
-        print(f"Loading part {part}")
-        wget.download(URL_LINKS[part], str(arch_path))
-        shutil.unpack_archive(arch_path, self._data_dir)
-        for fpath in (self._data_dir / "LibriSpeech").iterdir():
-            shutil.move(str(fpath), str(self._data_dir / fpath.name))
-        os.remove(str(arch_path))
-        shutil.rmtree(str(self._data_dir / "LibriSpeech"))
-
     def _get_or_load_index(self, part):
         index_path = self._data_dir / f"{part}_index.json"
         if index_path.exists():
